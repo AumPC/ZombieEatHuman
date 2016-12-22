7e6297f7a1d4d408f62fa8e62fa8a70f6f78e6be
AumPC <blue-aum@hotmail.com>
2016-12-22 17:56:23 +0700

    Human run

--- models.py
+++ models.py
@@ -18,8 +18,10 @@ class World:
         self.width = width
         self.height = height
         self.zombie = Zombie(self,100, 100)
-        self.human = Human(self, 400, 400)
+        self.human = Human(self, 400, 400,0,0,self.zombie)
         self.score = 0
+
+        self.human.random_direction()
 ##        self.asteroids = []
 ##        for i in range(World.NUM_ASTEROID):
 ##            asteroid = Asteroid(self, 0, 0, 0, 0)
@@ -28,9 +30,13 @@ class World:
  
     def animate(self, delta):
         self.zombie.animate(delta)
+        self.human.animate(delta)
         if self.zombie.hit(self.human, 15):
             self.human.random_location()
             self.score += 1
+            self.human.random_direction()
+
+            
 ##        for asteroid in self.asteroids:
 ##            asteroid.animate(delta)
 ##            if self.ship.hit(asteroid, 10):
@@ -60,12 +66,13 @@ class World:
         if key == arcade.key.DOWN:
             self.zombie.DIRECTION[3] = 0
 
+
 class Zombie(Model):
     DIRECTION = [0,0,0,0]  # LEFT RIGHT UP DOWN
     DIR_PIC = 1
     SPEED_X = 0
     SPEED_Y = 0
- 
+    xkeep = 650;
     def __init__(self, world, x, y):
         self.world = world
         super().__init__(world, x, y, 0)
@@ -98,33 +105,51 @@ class Zombie(Model):
         if self.x < 0:
             self.x = self.world.width
         self.x += self.SPEED_X
+        self.xkeep = self.x
 
 class Human(Model):
-    def __init__(self, world, x, y):
+    def __init__(self, world, x, y,vx,vy,zombie):
         super().__init__(world, x, y, 0)
+        self.vx = vx
+        self.vy = vy
+        self.zombie = zombie
+
+    def random_direction(self):
+        self.vx = 10 * random()
+        self.vy = 10 * random()
 
     def random_location(self):
         self.x = randint(0, self.world.width - 1)
         self.y = randint(0, self.world.height - 1)
 
-##class Asteroid(Model):
-##    def __init__(self, world, x, y, vx, vy):
-##        super().__init__(world, x, y, 0)
-##        self.vx = vx
-##        self.vy = vy
-##        self.angle = randint(0,359)
-##
-##    def random_direction(self):
-##        self.vx = 5 * random()
-##        self.vy = 5 * random()
-##
-##    def animate(self, delta):
+    def animate(self, delta):
 ##        if (self.x < 0) or (self.x > self.world.width):
 ##            self.vx = - self.vx
-##        
 ##        if (self.y < 0) or (self.y > self.world.height):
 ##            self.vy = - self.vy
-##        
-##        self.x += self.vx
-##        self.y += self.vy
-##        self.angle += 3
+        if self.y > self.world.height:
+            self.y = 0
+        if self.y < 0:
+            self.y = self.world.height
+        if self.x > self.world.width:
+            self.x = 0
+        if self.x < 0:
+            self.x = self.world.width
+
+        if (self.x > self.zombie.x-100) and (self.x < self.zombie.x) and (self.y > self.zombie.y-100) and (self.y < self.zombie.y+100):
+            if self.vx > 0:
+                self.vx = - self.vx
+        elif (self.x > self.zombie.x) and (self.x < self.zombie.x+100) and (self.y > self.zombie.y-100) and (self.y < self.zombie.y+100):
+            if self.vx < 0:
+                self.vx = - self.vx
+        if (self.x > self.zombie.x-100) and (self.x < self.zombie.x+100) and (self.y > self.zombie.y-100) and (self.y < self.zombie.y):
+            if self.vy > 0:
+                self.vy = - self.vy
+        elif (self.x > self.zombie.x-100) and (self.x < self.zombie.x+100) and (self.y < self.zombie.y+100) and (self.y > self.zombie.y):
+            if self.vy < 0:
+                self.vy = - self.vy
+
+        self.x += self.vx   
+        self.y += self.vy
+
+
